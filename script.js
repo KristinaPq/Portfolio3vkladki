5c9edd98907013dea81e9a95d35ed5cd46eb35e3
KristinaPV <109619349+KristinaPq@users.noreply.github.com>
2023-04-26 10:47:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var tabs = document.getElementById('tabs');
+		createTabs(tabs);
+		function createTabs(root) {
+			var items = root.getElementsByClassName('tabs__item');
+			var texts = root.getElementsByClassName('tabs__text');
+			root.addEventListener('click', go);
+			function go(event) {
+				var targ = event.target;
+				if (targ.tagName !== 'LI') return;
+				var select = 'tabs__item-select';
+				for (var i = 0; i < items.length; i++) {
+					items[i].classList.remove('select');
+				}
+				targ.classList.add('select');
+				for (var i = 0; i < texts.length; i++) {
+					texts[i].style.display = 'none'
+				} 
+				var id = targ.getAttribute('data-id');;
+				root.querySelector('div[id=' + id + ']').style.display = 'block';               
+			}
+		};
